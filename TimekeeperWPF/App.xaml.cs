6e61b338fb3a4ce9f38ef55ec15d4fecdc112377
NuCode1497 <neuburgercody@gmail.com>
2017-08-21 19:30:10 -0700

    Added ExceptionViewer, DateTimePicker

--- TimekeeperWPF/App.xaml.cs
+++ TimekeeperWPF/App.xaml.cs
@@ -4,7 +4,9 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using System.Windows;
+using TimekeeperWPF.Tools;
 
 namespace TimekeeperWPF
 {
@@ -13,5 +15,18 @@ namespace TimekeeperWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            //if (e.Exception.StackTrace.Contains("at Xceed.Wpf.Toolkit.Spinner.OnSpin(SpinEventArgs e)"))
+            //{
+            //    //Handle these stupid errors I'm not buying Plus for $500 are you kidding me
+            //    e.Handled = true;
+            //    return;
+            //}
+
+            ExceptionViewer ev = new ExceptionViewer("An unexpected error occurred in the application.", e.Exception, sender as Window);
+            ev.ShowDialog();
+            e.Handled = true;
+        }
     }
 }
